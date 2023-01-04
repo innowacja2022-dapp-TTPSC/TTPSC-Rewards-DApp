12596af23753d0af19c602b3ac7a0aee7a02b129
MaciejIwan <64867638+MaciejIwan@users.noreply.github.com>
2023-01-04 17:03:14 +0100

    add token contract example

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,9 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      chainId: 31337
+    }
+  }
 };
