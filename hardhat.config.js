f934edf72997ad037b7c2cd5cf718b06283791b1
Adam <adamj1215@gmail.com>
2023-01-06 15:56:16 +0100

    can connect to mumbai, added dotenv dependency

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,24 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 // The next line is part of the sample project, you don't need it in your
 // project. It imports a Hardhat task definition, that can be used for
 // testing the frontend.
 require("./tasks/faucet");
 
+const {MUMBAI_RPC_URL, PRIVATE_KEY} = process.env;
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
     solidity: "0.8.17",
+    defaultNetwork: "mumbai",
     networks: {
         hardhat: {
             chainId: 1337
+        },
+        mumbai: {
+            url: MUMBAI_RPC_URL,
+            accounts: [PRIVATE_KEY]
         }
     }
 };
