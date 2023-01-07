1bb15679b0b627eb66d78cff20af60166954befa
MaciejIwan <64867638+MaciejIwan@users.noreply.github.com>
2023-01-07 22:03:56 +0100

    add PaymentsManager.sol

---
+++ test/PaymentsManager.js
@@ -0,0 +1,19 @@
+const {expect} = require("chai");
+
+
+const {loadFixture} = require("@nomicfoundation/hardhat-network-helpers");
+const {ethers} = require("hardhat");
+
+
+describe("Token contract", function () {
+    //TODO write tests
+    async function deployTokenFixture() {
+
+    }
+
+    // You can nest describe calls to create subsections.
+    describe("Deployment", function () {
+
+    });
+
+});
