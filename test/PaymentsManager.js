18afbcc804570635777d4261fea38b7d278a666e
Adam <adamj1215@gmail.com>
2023-01-21 00:21:22 +0100

    refatored previous test code a bit, deleted unnecessary test files

--- test/PaymentsManager.js
+++ test/PaymentsManager.js
@@ -2,30 +2,35 @@ const {expect} = require("chai");
 const {ethers} = require("hardhat");
 
 
-describe("Payments manager contract", function () {
+describe("Payments manager contract", () => {
 
     let paymentsManager;
     let ttpscToken;
     let employeeAddress;
 
-    before(async function() {
+    before(async () => {
         const token = await ethers.getContractFactory("TTPSC");
-        const contractArgs = {TOKEN_NAME: "TTPSC token", TOKEN_SYMBOL: "TTPSC", INITIAL_SUPPLY: ethers.utils.parseUnits("10", "ether")}
+        const contractArgs = {TOKEN_NAME: "TTPSC token", TOKEN_SYMBOL: "TTPSC", INITIAL_SUPPLY: ethers.utils.parseUnits("10", "ether")};
+
         ttpscToken = await token.deploy(
             contractArgs.TOKEN_NAME,
             contractArgs.TOKEN_SYMBOL,
             contractArgs.INITIAL_SUPPLY
         );
+
         const manager = await ethers.getContractFactory("PaymentsManager");
+
         // ADDR1 IS FOR TESTING FUNCTIONS THAT REQUIRE EMPLOYEE STATUS
         const [owner, addr1] = await ethers.getSigners();
         paymentsManager = await manager.deploy(ttpscToken.address);
+
         await ttpscToken.transfer(paymentsManager.address, contractArgs.INITIAL_SUPPLY);
         employeeAddress = addr1;
+
         await paymentsManager.hireEmployee(employeeAddress.address);
     });
 
-    describe("Deployment", function () {
+    describe("Deployment", () => {
         it("Should set the right token address", async function() {
             const tokenAddressInManager = await paymentsManager.token();
             expect(tokenAddressInManager).to.equal(ttpscToken.address);
@@ -37,22 +42,22 @@ describe("Payments manager contract", function () {
         });
     });
 
-    describe("Employee and employers handling", function() {
-        it("Should hire new employer", async function() {
+    describe("Employee and employers handling", () => {
+        it("Should hire new employer", async () => {
             const randomWalletAddress = ethers.Wallet.createRandom().address;
             await paymentsManager.hireEmployer(randomWalletAddress);
             const isEmployer = await paymentsManager.isEmployer(randomWalletAddress);
             expect(isEmployer).to.equal(true);
         });
 
-        it("Should hire new employee", async function() {
+        it("Should hire new employee", async () => {
             const randomWalletAddress = ethers.Wallet.createRandom().address;
             await paymentsManager.hireEmployee(randomWalletAddress);
             const isEmployer = await paymentsManager.isEmployer(randomWalletAddress);
             expect(isEmployer).to.equal(false);
         });
 
-        it("Should fire employee", async function() {
+        it("Should fire employee", async () => {
             const randomWalletAddress = ethers.Wallet.createRandom().address;
             const [x] = await ethers.getSigners();
             await paymentsManager.hireEmployee(randomWalletAddress);
@@ -66,7 +71,7 @@ describe("Payments manager contract", function () {
             expect(isCurrentEmployee).to.equal(false);
         });
 
-        it("Should throw appriopriate error if handling employees as an employee", async function() {
+        it("Should throw appriopriate error if handling employees as an employee", async () => {
             const randomWalletAddress = ethers.Wallet.createRandom().address;
             await expect(paymentsManager.connect(employeeAddress).hireEmployer(randomWalletAddress))
                 .to.be.revertedWith("Only the employer can add employees.");
@@ -77,12 +82,12 @@ describe("Payments manager contract", function () {
         });
     });
 
-    describe("Token reclaiming", function() {
+    describe("Token reclaiming", () => {
         // TODO
     });
 
-    describe("Payment requests", function() {
-        it("Should create a payment request with correct parameters", async function() {
+    describe("Payment requests", () => {
+        it("Should create a payment request with correct parameters", async () => {
             const reason = "For completing task";
             const amount = 1;
             const expectedStatus = 0;
@@ -98,7 +103,7 @@ describe("Payments manager contract", function () {
             expect(requestStatus).to.equal(expectedStatus);
         });
 
-        it("Should accept a payment request and transfer token to employee's account", async function() {
+        it("Should accept a payment request and transfer token to employee's account", async () => {
             const reason = "For completing task";
             const amount = 10;
             const expectedStatus = 1;
@@ -112,7 +117,7 @@ describe("Payments manager contract", function () {
             expect(await ttpscToken.balanceOf(employeeAddress.address)).to.equal(amount);
         });
 
-        it("Should reject a payment request", async function() {
+        it("Should reject a payment request", async () => {
             const reason = "For completing task";
             const amount = 1;
             const expectedStatus = 2;
@@ -125,12 +130,12 @@ describe("Payments manager contract", function () {
             expect(requestStatus).to.equal(expectedStatus);
         });
 
-        it("Should throw error if creating payment request as an employer", async function() {
+        it("Should throw error if creating payment request as an employer", async () => {
             await expect(paymentsManager.createPaymentRequest(1, "For completing task"))
                 .to.be.revertedWith("Only employees can create paymentRequests.");
         });
 
-        it("Should throw error if accepting or rejecting payment request as an employee", async function() {
+        it("Should throw error if accepting or rejecting payment request as an employee", async () => {
             const requestId = await paymentsManager.paymentRequestCount();
             await paymentsManager.connect(employeeAddress).createPaymentRequest(1, "For completing task");
             await expect(paymentsManager.connect(employeeAddress).acceptPaymentRequest(requestId, "Ok"))
@@ -139,7 +144,7 @@ describe("Payments manager contract", function () {
                 .to.be.revertedWith("Only employers can decide on paymentRequests .");
         });
 
-        it("Should throw error if trying to change already established payment request status", async function() {
+        it("Should throw error if trying to change already established payment request status", async () => {
             const requestId = await paymentsManager.paymentRequestCount();
             await paymentsManager.connect(employeeAddress).createPaymentRequest(1, "For completing task");
             await paymentsManager.acceptPaymentRequest(requestId, "Task is well done");
@@ -147,8 +152,8 @@ describe("Payments manager contract", function () {
                 .to.be.revertedWith("This paymentRequest has already been decided on.");
         });
 
-        it("Should return all created payment requests ids", async function() {
-            // IT WILL RETURN ALL REQUESTS CREATED WITHIN PREVIOUS TESTS (SO 5)
+        // IT WILL RETURN ALL REQUESTS CREATED WITHIN PREVIOUS TESTS (SO 5)
+        it("Should return all created payment requests ids", async () => {
             const expectedRequestsAmount = 5;
             const requests = await paymentsManager.getPaymentRequestHistory();
             expect(requests.length).to.equal(expectedRequestsAmount);
---
+++ test/RewardsManager.js
@@ -0,0 +1,9 @@
+
+const {expect} = require("chai");
+const {ethers} = require("hardhat");
+
+
+describe("Rewards manager contract", async () => {
+
+    
+});
